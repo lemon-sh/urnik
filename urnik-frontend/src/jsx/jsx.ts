20e2b0a712a31fc1f60cff8b0ef0301227d18118
Skwodo <skwodo@skwodo.com>
2024-07-15 20:15:38 +0200

    implement jsx

---
+++ urnik-frontend/src/jsx/jsx.ts
@@ -0,0 +1,23 @@
+/// <reference lib="DOM" />
+
+declare namespace JSX {
+   // The return type of our JSX Factory
+   type Element = HTMLElement;
+
+   // IntrinsicElementMap grabs all the standard HTML tags in the TS DOM lib.
+   interface IntrinsicElements extends IntrinsicElementMap { }
+
+   // The following are custom types, not part of TS's known JSX namespace:
+   type IntrinsicElementMap = {
+       [K in keyof HTMLElementTagNameMap]: {
+           [k: string]: any
+       }
+   }
+
+   type Tag = keyof JSX.IntrinsicElements;
+
+   interface Component {
+       (properties?: { [key: string]: any }, children?: Node[]): Node
+   }
+}
+
