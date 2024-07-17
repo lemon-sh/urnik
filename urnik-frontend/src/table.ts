29c0ffcff0595e0701e3ea7c97bbbcf030d19060
Skwodo <skwodo@skwodo.com>
2024-07-17 23:46:47 +0200

    Revert "implement jsx"
    This reverts commit 20e2b0a712a31fc1f60cff8b0ef0301227d18118.

--- urnik-frontend/src/table.ts
+++ urnik-frontend/src/table.ts
@@ -1,5 +1,4 @@
 import { Schedule, Interval, Lesson, isDivision, Division, isTeacher, Teacher, Room } from "./types";
-import jsx from './jsx/jsxFactory';
 
 const tableHeaders = ["Nr", "Godz", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek"] as const;
 
@@ -34,35 +33,32 @@ export function modifyTable(table: HTMLTableElement, schedule: Schedule, interva
         newRow.cells[1].textContent = `${intervals[i][0]} - ${intervals[i][1]}`;
     }
 
-    let lessonContent: (lesson: Lesson) => HTMLElement;
-    const toSpan = (str: string, type: string) => <span class={type}>{str}</span>;
-    const toAnchor = (str: string, type: string) => <a href={`#/${type}/${encodeURIComponent(str)}`} class={type}>{str}</a>;
+    let lessonContent: (lesson: Lesson) => string;
+    const toSpan = (str: string, type: string) => `<span class="${type}">${str}</span>`;
+    const toAnchor = (str: string, type: string) => `<a href="#/${type}/${encodeURIComponent(str)}" class="${type}">${str}</a>`;
 
     let lesson = schedule[0]
     if (isDivision(lesson)) {
-        lessonContent = (l) => <>
-            {toSpan(l.subject, 'p')}
-            {l.group ? `-${l.group}` : ""}&nbsp;
-            {toAnchor((l as Division).room, 's')}&nbsp;
-            {toAnchor((l as Division).teacher, 'n')}<br />
-        </>;
+        lessonContent = (l) => 
+            `${toSpan(l.subject, 'p')} 
+            ${l.group ? `-${l.group}` : ""} 
+            ${toAnchor((l as Division).room, 's')} 
+            ${toAnchor((l as Division).teacher, 'n')}<br>`;
     } else if (isTeacher(lesson)) {
-        lessonContent = (l) => <>
-            {toAnchor((l as Teacher).division, 'o')}
-            {l.group ? `-${l.group}` : ""}&nbsp;
-            {toSpan(l.subject, 'p')}&nbsp;
-            {toAnchor((l as Teacher).room, 's')}<br />
-        </>;
+        lessonContent = (l) =>
+            `${toAnchor((l as Teacher).division, 'o')} 
+            ${l.group ? `-${l.group}` : ""} 
+            ${toSpan(l.subject, 'p')} 
+            ${toAnchor((l as Teacher).room, 's')}<br>`;
     } else {
-        lessonContent = (l) => <>
-            {toAnchor((l as Room).division, 'o')}
-            {l.group ? `-${l.group}` : ""}&nbsp;
-            {toSpan(l.subject, 'p')}&nbsp;
-            {toAnchor((l as Room).teacher, 'n')}<br />;
-        </>;
-    }
+        lessonContent = (l) => 
+            `${toAnchor((l as Room).division, 'o')} 
+            ${l.group ? `-${l.group}` : ""} 
+            ${toSpan(l.subject, 'p')} 
+            ${toAnchor((l as Room).teacher, 'n')}<br>`;
+    } 
 
     for (lesson of schedule) {
-        tBody.rows[lesson.interval_id].cells[lesson.day + 2].appendChild(lessonContent(lesson));
+        tBody.rows[lesson.interval_id].cells[lesson.day + 2].innerHTML += lessonContent(lesson);
     }
 }
